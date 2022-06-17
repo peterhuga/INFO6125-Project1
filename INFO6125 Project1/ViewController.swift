6a406f80658b418c896b9f7f5417cb56d5eebbed
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-06-17 15:51:08 -0400

    Something done on KB.

--- INFO6125 Project1/ViewController.swift
+++ INFO6125 Project1/ViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var bottomRowButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
 
 
 }
